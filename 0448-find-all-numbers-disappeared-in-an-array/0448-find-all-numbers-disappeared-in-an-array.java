7e58c2fb267e8bc63375055ed5604fc1f88da042
Rohit Shrivastava <162025853+rohit29032005@users.noreply.github.com>
2026-08-15 00:04:50 +0530

    Refactor findDisappearedNumbers method implementation
    Refactored the findDisappearedNumbers method to use a HashSet for tracking missing numbers instead of modifying the input array.

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,21 +1,18 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        List<Integer> arr = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
-            int index = Math.abs(nums[i]) - 1;
+    int num = nums[i];
+}
 
-            if (nums[index] > 0) {
-                nums[index] = -nums[index];
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) {
+                ans.add(i);
             }
         }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                arr.add(i + 1);
-            }
-        }
-
-        return arr;
+        return ans;
     }
 }
